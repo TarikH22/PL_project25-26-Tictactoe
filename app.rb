ea612a8f23879a360037880a8562041940c6b4b6
tarikhamzic <tarikhamzic22@icloud.com>
2025-12-29 14:38:33 +0100

    Add bot opponent feature with game mode selection

--- app.rb
+++ app.rb
@@ -28,6 +28,44 @@ class TicTacToe
     # Game still in progress
     nil
   end
+
+  # Simple bot AI
+  def self.bot_move(board, bot_symbol)
+    player_symbol = bot_symbol == 'X' ? 'O' : 'X'
+    
+    # Strategy 1: Win if possible
+    move = find_winning_move(board, bot_symbol)
+    return move if move
+    
+    # Strategy 2: Block opponent from winning
+    move = find_winning_move(board, player_symbol)
+    return move if move
+    
+    # Strategy 3: Take center if available
+    return 4 if board[4].nil?
+    
+    # Strategy 4: Take a corner
+    corners = [0, 2, 6, 8]
+    available_corners = corners.select { |i| board[i].nil? }
+    return available_corners.sample if available_corners.any?
+    
+    # Strategy 5: Take any available spot
+    board.each_with_index do |cell, index|
+      return index if cell.nil?
+    end
+    
+    nil
+  end
+
+  def self.find_winning_move(board, symbol)
+    WINNING_COMBOS.each do |combo|
+      values = combo.map { |i| board[i] }
+      if values.count(symbol) == 2 && values.count(nil) == 1
+        return combo[values.index(nil)]
+      end
+    end
+    nil
+  end
 end
 
 # Initialize game state in session if not present
@@ -36,6 +74,7 @@ before do
   session[:current_player] ||= 'X'
   session[:scores] ||= { 'X' => 0, 'O' => 0 }
   session[:winner] ||= nil
+  session[:game_mode] ||= '2player'
 end
 
 # Routes
@@ -44,10 +83,19 @@ get '/' do
 end
 
 get '/game' do
+  # Set game mode if provided in params
+  if params[:mode]
+    session[:board] = Array.new(9, nil)
+    session[:current_player] = 'X'
+    session[:winner] = nil
+    session[:game_mode] = params[:mode]
+  end
+  
   @board = session[:board]
   @current_player = session[:current_player]
   @scores = session[:scores]
   @winner = session[:winner]
+  @game_mode = session[:game_mode]
   erb :game
 end
 
@@ -56,12 +104,19 @@ get '/move/:index' do
   index = params[:index].to_i
   board = session[:board]
   player = session[:current_player]
+  game_mode = session[:game_mode]
 
   # Ignore if cell already taken or game over
   if board[index] || session[:winner]
     redirect '/game'
   end
 
+  # In bot mode, only allow player X to make moves
+  if game_mode == 'bot' && player == 'O'
+    redirect '/game'
+  end
+
+  # Make player move
   board[index] = player
   session[:board] = board
   result = TicTacToe.check_winner(board)
@@ -71,6 +126,23 @@ get '/move/:index' do
     session[:scores][result[:winner]] += 1 if result[:winner] != 'draw'
   else
     session[:current_player] = player == 'X' ? 'O' : 'X'
+    
+    # If bot mode and it's now the bot's turn, make bot move
+    if game_mode == 'bot' && session[:current_player] == 'O' && !session[:winner]
+      bot_index = TicTacToe.bot_move(board, 'O')
+      if bot_index
+        board[bot_index] = 'O'
+        session[:board] = board
+        bot_result = TicTacToe.check_winner(board)
+        
+        if bot_result
+          session[:winner] = bot_result[:winner]
+          session[:scores][bot_result[:winner]] += 1 if bot_result[:winner] != 'draw'
+        else
+          session[:current_player] = 'X'
+        end
+      end
+    end
   end
   redirect '/game'
 end
@@ -81,5 +153,6 @@ post '/api/reset' do
   session[:board] = Array.new(9, nil)
   session[:current_player] = 'X'
   session[:winner] = nil
+  # Keep the game mode for next game
   json({ status: 'success' })
 end
